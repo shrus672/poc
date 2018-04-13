7099fa98c9f98fb7af3aec90e70ad794acb2a05d
IS-Nikhilesh Tiwari (PSP) <ntiwari@ntiwari007271.starbucks.net>
2018-04-13 03:08:55 -0700

    first commit

---
+++ ios/fastlanepocUITests/SnapshotHelper.swift
@@ -0,0 +1,9 @@
+//
+//  SnapshotHelper.swift
+//  fastlanepocUITests
+//
+//  Created by IS-Nikhilesh Tiwari (PSP) on 4/4/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
